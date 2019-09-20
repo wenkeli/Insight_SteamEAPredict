0736edf57bb8f6affc7df2ed2ae903561efc592d
consciousness <wenke.li@utexas.edu>
2019-09-20 14:12:22 -0400

    added code to collect the app news together and format the tables

---
+++ makeAppNewsTable.py
@@ -0,0 +1,30 @@
+import json
+import datetime
+import os
+
+import pandas as pd
+import numpy as np
+
+appNewsDir="appNews"
+
+fileNames=os.listdir(appNewsDir)
+
+allNews=pd.DataFrame()
+for fileName in fileNames:
+    print(fileName)
+    fPath=os.path.join(appNewsDir, fileName)
+    
+    try:
+        with open(fPath, "r") as fh:
+            data=json.load(fh)
+        data=pd.DataFrame(data["appnews"]["newsitems"])
+        data=data[["title", "author", "feedlabel", "feedname", "appid", "date"]]
+        allNews=allNews.append(data)
+    except:
+        continue;
+
+allNewsFN="appNews/allNews.json"
+
+allNews.reset_index(inplace=True)
+allNews.drop(["index"], 1, inplace=True)
+allNews.to_json(allNewsFN)
