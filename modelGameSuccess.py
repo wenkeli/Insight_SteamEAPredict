5ea8e647dbcc6485fa62d068278bef8d3f26c6ec
consciousness <wenke.li@utexas.edu>
2019-09-25 23:18:50 -0400

    added functions to split data into training and testing set and applying models to the sets

--- modelGameSuccess.py
+++ modelGameSuccess.py
@@ -1,45 +1,67 @@
-import urllib
-import copy
-import datetime
+import os
 
 import pandas as pd
-import json
 import numpy as np
 
-import matplotlib;
-matplotlib.use("pdf");
-
-import matplotlib.pyplot as pp;
-import matplotlib.backends.backend_pdf as pdf;
-import matplotlib.gridspec as gs;
-
-import seaborn as sns
-
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
-matplotlib.rcParams['pdf.fonttype'] = 42;
-matplotlib.rcParams['ps.fonttype'] = 42;
+def makeTrainValSets(df, catCol, nSplits=4):
+    splitFrac=1/nSplits
+    cats=df[catCol].unique()
+    df=df.set_index(catCol)
+
+    valSet=dict()
+    fitSet=dict()
+
+    nRows=dict()
+    randInds=dict()
+    for cat in cats:
+        nRows[cat]=len(df.loc[cat])
+        randInds[cat]=np.random.permutation(np.arange(nRows[cat]))
+
+        valSet[cat]=[]
+        fitSet[cat]=[]
+
+
+    for i in np.arange(nSplits):
+        startF = i * splitFrac
+        endF = (i + 1) * splitFrac
+        for cat in cats:
+            startN=int(startF*nRows[cat])
+            endN=int(endF*nRows[cat])
+            valInds=randInds[cat][startN:endN]
+            fitInds=list(set(randInds[cat])-set(valInds))
+            valSet[cat].append(df.loc[cat].iloc[valInds].reset_index())
+            fitSet[cat].append(df.loc[cat].iloc[fitInds].reset_index())
+
+    return {"val": valSet, "fit": fitSet}
+
+
+def trainTest(df, model, nSplits=4):
+    dataSets = makeTrainValSets(df, "category", nSplits=nSplits)
+    for i in np.arange(nSplits):
+        fitSet = dataSets["fit"]["fail"][i]
+        fitSet = fitSet.append(dataSets["fit"]["success"][i])
+        fitSetCols=fitSet.columns
+        model.fit(fitSet.drop(["appID", "category", "refTS"], 1), fitSet["category"])
+
+        testSet = dataSets["val"]["fail"][i]
+        testSet = testSet.append(dataSets["val"]["success"][i])
+        testSet=testSet[fitSetCols].copy()
+        testRes = testSet[["category"]].copy()
+        pred = model.predict(testSet.drop(["appID", "category", "refTS"], 1))
+        testRes["predict"] = pred
+        print(testRes.groupby(["category", "predict"]).size())
+
     
+appFeatureDir="appFeatures"
 
-appAges=pd.read_json("appAttrs/appAges.json", encoding="utf-8")
+app90Days=pd.read_json(os.path.join(appFeatureDir, "90Days.json"))
 
-failSet=appAges.set_index(["status"]).loc["active"].reset_index()
-failSet["fail"]=failSet["Now-R"]>(365*1.5)
-failSet=failSet.set_index(["fail"]).loc[True].reset_index().drop(["fail"], 1)
+app180Days=pd.read_json(os.path.join(appFeatureDir, "180Days.json"))
 
-successSet=appAges.set_index(["status"]).loc["finished"].reset_index()
-successSet["success"]=(successSet["R-FN"]>90) & (successSet["R-FN"]<(365*1.5))
-successSet=successSet.set_index(["success"]).loc[True].reset_index().drop(["success"], 1)
+app300Days=pd.read_json(os.path.join(appFeatureDir, "300Days.json"))
 
-appNIStats=pd.read_json("appAttrs/appNewsStats.json", encoding="utf-8")
-
-failSet=failSet.merge(appNIStats[["nNews", "mean", "std", "max", "appID"]], on="appID")
-failSet["max"]=list(map(lambda m1, m2: np.max([m1, m2]), failSet["max"], failSet["now-LN"]))
-
-successSet=successSet.merge(appNIStats[["nNews", "mean", "std", "max", "appID"]])
-
-workingSet=failSet.append(successSet)
-
-clf=RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
-
-clf.fit(workingSet[["mean", "nNews", "max"]], workingSet["status"])
+rfC = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0, class_weight="balanced_subsample")
+logC = LogisticRegression(class_weight="balanced", solver="lbfgs")
