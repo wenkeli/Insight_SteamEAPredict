53cd21512a5f8daac677e7c4855891f250f75571
consciousness <wenke.li@utexas.edu>
2019-09-25 23:11:17 -0400

    changed the time now to 2019-09-21

--- calcAppAge.py
+++ calcAppAge.py
@@ -26,64 +26,14 @@ matplotlib.rcParams['ps.fonttype'] = 42;
 #         return firstNewsTS
     
 
-appListFN="appAttrs/appTable.json"
-allNewsFN="appNews/allNews.json"
+appListFN="appAttrs/appTableClean.json"
 
 appList=pd.read_json(appListFN, encoding="utf-8")
 
-appNews=pd.read_json(allNewsFN, encoding="utf-8")
-appNews["timeS"]=list(map(lambda d: d.timestamp(), appNews["date"]))
-
-appFNTS=appNews.groupby(["appid"]).min()["timeS"].reset_index()
-appFNTS.rename(columns={"appid": "appID", "timeS": "FNTS"}, inplace=True)
-
-appLNTS=appNews.groupby(["appid"]).max()["timeS"].reset_index()
-appLNTS.rename(columns={"appid": "appID", "timeS": "LNTS"}, inplace=True)
-
-appList=appList.merge(appFNTS)
-appList=appList.merge(appLNTS)
-
 nSPDay=3600*24
-appList["R-FN"]=(appList["rTimeS"]-appList["FNTS"])/nSPDay
 
-nowTS=datetime.datetime.utcnow().timestamp()
+nowTS=datetime.datetime(2019, 9, 21).timestamp()
 appList["Now-R"]=(nowTS-appList["rTimeS"])/nSPDay
-appList["now-LN"]=(nowTS-appList["LNTS"])/nSPDay
 
+appList=appList[["Now-R", "appID"]].copy()
 appList.to_json("appAttrs/appAges.json")
-
-
-appList=pd.read_json("appAttrs/appAges.json", encoding="utf-8")
-plotDir="plots"
-
-appsByStat=appList.set_index(["status"])
-
-pages=pdf.PdfPages("plots/appAge_distributions.pdf")
-fig=pp.figure(figsize=(5, 3))
-ax=fig.add_subplot(111)
-plotD=np.array(appsByStat.loc["active", "Now-R"])
-plotD=plotD[(plotD<3000) & (plotD>-50)]
-sns.distplot(plotD, hist=True, color="r", kde=False, ax=ax)
-ax.set_xlabel("days")
-fig.suptitle("age of Early Access games")
-pages.savefig(fig)
-pp.close(fig)
-fig=pp.figure(figsize=(5, 3))
-ax=fig.add_subplot(111)
-plotD=np.array(appsByStat.loc["finished", "R-FN"])
-plotD=plotD[(plotD<2500) & (plotD>-50)]
-sns.distplot(plotD, hist=True, color="b", kde=False, ax=ax)
-ax.set_xlabel("days")
-fig.suptitle("finished games: first news to release")
-pages.savefig(fig)
-pp.close(fig)
-fig=pp.figure(figsize=(5, 3))
-ax=fig.add_subplot(111)
-plotD=np.array(appsByStat.loc["active", "now-LN"])
-plotD=plotD[(plotD<1500) & (plotD>-50)]
-sns.distplot(plotD, hist=True, color="r", kde=False, ax=ax)
-ax.set_xlabel("days")
-fig.suptitle("early access games: age of last news")
-pages.savefig(fig)
-pp.close(fig)
-pages.close()
